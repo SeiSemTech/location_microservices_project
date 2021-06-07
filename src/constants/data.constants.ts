bed371fba5580e783ce0fb03c2c8e993b88fd617
Sergio Portilla <sergio.portilla@scotiabank.com.co>
2021-06-07 02:36:02 -0500

    add constants

--- src/constants/data.constants.ts
+++ src/constants/data.constants.ts
@@ -1,2 +1,5 @@
-export const EARTH = 6371; //Km
-export const RADIAN = 180 //π
+export const EARTH: number = 6371; //Km
+export const RADIAN: number = 180; //π
+export const ONE: number = 1;
+export const TWO: number = 2;
+export const THOUSAND: number = 1000;
